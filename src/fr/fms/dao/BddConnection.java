5fc06fff851c5238c67a18ee617feeef167cca0b
Claire AVILA <avila.claire@gmail.com>
2024-02-06 14:50:55 +0100

    gestion de la connexion avec la BDD

--- src/fr/fms/dao/BddConnection.java
+++ src/fr/fms/dao/BddConnection.java
@@ -1,23 +1,58 @@
 package fr.fms.dao;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 
 public class BddConnection {
 
+	private static Connection connection = null;
+	private static String driver;
+	private static String url;
+	private static String login;
+	private static String password;
 
-		static Connection getConnection() {
-			try  {
-				Class.forName("org.mariadb.jdbc.Driver");
-				Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/Shop","root","fms2024");
-				return connection;
-				
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-			return null;
+	private static final Logger logger = Logger.getLogger(BddConnection.class.getName());
+
+
+	//on sécurise la connection et on la met en place
+	private BddConnection() {
+		try  {
+			Class.forName(driver);
+			Connection connection = DriverManager.getConnection(url,login,password);
+		}			
+		catch (ClassNotFoundException | SQLException e) {
+			logger.severe("connection pb : " + e.getMessage());
+
 		}
-
-		
+		catch (Exception e) {
+			logger.severe("pb : " + e.getMessage());
+		}
+	}
+	//singleton pour véifier que la connection existe
+	public static synchronized Connection getConnection() {	
+		if(connection == null) 	new BddConnection();
+		return connection;
 	}
 
 
+	private static void getConfigFile() throws FileNotFoundException, IOException {
+		Properties props = new Properties();		
+		try (FileInputStream fis = new FileInputStream("files/config.properties")){
+			props.load(fis);
+		} catch (FileNotFoundException e1) {
+			logger.severe("Fichier de config non trouvé " + e1.getMessage());
+		} catch (IOException e1) {
+			logger.severe("Erreur lecture fichier config " + e1.getMessage());
+		}		
+		driver = props.getProperty("db.driver");
+		url = props.getProperty("db.url");
+		login = props.getProperty("db.login");
+		password = props.getProperty("db.password");
+	}
+}
--- src/fr/fms/dao/CreateConfigFile.java
+++ src/fr/fms/dao/CreateConfigFile.java
@@ -1,5 +1,38 @@
 package fr.fms.dao;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 
 public class CreateConfigFile {
 
+
+
+	public static void main(String args[]) throws IOException {
+		
+		Properties prop = readPropertiesFile("src/config.properties");
+		System.out.println(prop.getProperty("db.driver\", \"org.mariadb.jdbc.Driver"));
+		System.out.println(prop.getProperty("db.url\", \"jdbc:mariadb://localhost:3306/formationdev"));
+		System.out.println( prop.getProperty("db.login","root"));
+		System.out.println(prop.getProperty("db.password","fms2024"));
+
+	}
+	public static Properties readPropertiesFile(String fileName) throws IOException {
+		FileInputStream fis = null;
+		Properties prop = null;
+		try {
+			fis = new FileInputStream(fileName);
+			prop = new Properties();
+			prop.load(fis);
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			fis.close();
+		}
+		return prop;
+	}
 }
+
