30be7e88c43c7aed2be0ad6631f345bc4928a51f
Claire AVILA <avila.claire@gmail.com>
2024-02-07 14:20:15 +0100

    création de la class order

--- src/fr/fms/dao/DaoFactory.java
+++ src/fr/fms/dao/DaoFactory.java
@@ -1,5 +1,34 @@
 package fr.fms.dao;
+import fr.fms.entities.Courses;
+import fr.fms.entities.Customer;
+import fr.fms.entities.Order;
+import fr.fms.entities.Cart;
+import fr.fms.entities.User;
 
 public class DaoFactory {
 
+	
+	public static Dao<Courses> getCoursesDao() {
+		return new CoursesDao();		
+	}
+	
+	public static Dao<User> getUserDao() {
+		return new UserDao();
+	}
+	
+ 	
+ 	public static Dao<Order> getOrderDao() {
+ 		return new OrderDao();
+ 	}
+ 	
+ 	public static Dao <Cart> getCartDao() {
+ 		return new CartDao();
+ 	}
+ 	
+ 	public static Dao<Customer> getCustomerDao() {
+ 		return new CustomerDao();
+ 	}
+ 	
+ 	
 }
+
